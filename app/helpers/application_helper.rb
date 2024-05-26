b195b3b3eef7e5ac133584b9228d91d64d5eef46
Jamerson Carlos <64469941+JamersonCarlos@users.noreply.github.com>
2024-05-26 05:17:15 -0300

    including helpers
    helpers for filtering uncompleted and completed tasks

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+    
+    def filter_completed(tasks)
+        tasks.filter { |task| task.completed }
+    end 
+
+    def filter_not_completed(tasks)
+        tasks.filter { |task| task.completed == false}
+    end
 end
