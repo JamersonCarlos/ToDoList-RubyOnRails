f68fcb1216e143df7f7bd994baf8a24d209fea59
Jamerson Carlos <64469941+JamersonCarlos@users.noreply.github.com>
2024-05-28 00:01:51 -0300

    helper responsible for grouping lists according to category

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,9 +8,13 @@ module ApplicationHelper
         tasks.filter { |task| task.completed == false}
     end
 
-
     def counter_tasks(id)
         Task.where(list_id: id).length
     end 
+
+    def agroup_category(lists)
+        lists.group_by { |item| item[:category]}
+
+    end
         
 end
