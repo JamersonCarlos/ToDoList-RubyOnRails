29893443f9aafe9f48c0c1ac9ae355147c4e1ab4
jamersoncarlos <jcventurosa@gmail.com>
2024-06-03 19:18:32 -0300

    update show action

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -9,7 +9,7 @@ class ListsController < ApplicationController
 
   # GET /lists/1 or /lists/1.json
   def show
-    @tasks = Task.where(list_id: @list.id)
+    @tasks = @list.tasks
   end
 
   # GET /lists/new
