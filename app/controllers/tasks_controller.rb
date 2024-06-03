72d129f9ee5ff6289d0dcea030f1381822528600
jamersoncarlos <jcventurosa@gmail.com>
2024-06-03 20:13:24 -0300

    validation in the task addition and editing form

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,19 +1,29 @@
 class TasksController < ApplicationController
-    before_action :set_list, only:  %i[index]
-    before_action :set_task, only: %i[destroy]
+    before_action :set_list, only:  %i[index create]
+    before_action :set_task, only: %i[destroy update]
 
     def index
         @task = Task.new
-        @tasks = Task.where(list_id: @list.id)
+        @tasks = @list.tasks
     end
 
     def create
-        @task = Task.new(list_params)
-        @task.list_id = params[:list_id]
-        if Task.where(title: @task.title) == []
-            @task.save
-        end 
-        redirect_to request.referrer
+        if list_params[:title].present? 
+            @task = Task.new(list_params)
+            @task.list = @list
+            respond_to do |format|
+                result = Task.where(title: @task.title)
+                if result == [] && @task.save
+                    format.html { redirect_to request.referrer}
+                else 
+                    format.html { redirect_to request.referrer, notice: "A task with the same name already exists!", error: true}
+                end 
+            end
+        else 
+            respond_to do |format|
+                format.html { redirect_to request.referrer, notice: "Does not accept Task with empty title", error: true }
+            end
+        end
     end
 
     def destroy
@@ -22,23 +32,32 @@ class TasksController < ApplicationController
     end
 
     def update 
-        @task = Task.where(id: params[:id].present? ? params[:id] : params[:task_id]) 
-        if params[:task].present? && params[:task][:title].present?
-            @task.update(title: params[:task][:title])
-        else 
-            @task.update(completed: params[:completed])
-            @task.update(done_at: Time.current)
+        respond_to do |format|
+            if params[:task].present? && params[:task][:title].present?
+                if Task.where(title: params[:task][:title]) == [] 
+                    @task.update(title: params[:task][:title])
+                    format.html { redirect_to request.referrer}
+                else 
+                    format.html { redirect_to request.referrer, notice: "A task with the same name already exists!"}
+                end 
+            elsif params[:completed].present?
+                @task.update(completed: params[:completed])
+                @task.update(done_at: Time.current)
+                format.html { redirect_to request.referrer}
+            else 
+                format.html { redirect_to request.referrer, notice: "Does not accept Task with empty title" }
+            end
         end
-        redirect_to request.referrer
+       
     end
         
     private 
         def set_list 
-            @list = List.where(id: params[:list_id])[0]
+            @list = List.find_by(id: params[:list_id])
         end
 
         def set_task
-            @task = Task.find_by(id: params[:task_id])
+            @task = Task.find_by(id: params[:task_id].present? ? params[:task_id] : params[:id])
         end
 
         def list_params 
