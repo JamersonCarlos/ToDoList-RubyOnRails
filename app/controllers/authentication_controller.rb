837d20abb0bbe179d4aeef18e1d62e799aaa1ec2
jamersoncarlos <jcventurosa@gmail.com>
2024-06-18 10:25:24 -0300

    specifying rendering layout for the register page

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -4,9 +4,14 @@ class AuthenticationController < ApplicationController
   end
 
   def register
+    render layout: "authentication_layout"
   end
 
   def signin 
+    redirect_to root_path
+  end
+
+  def create 
     redirect_to root_path
   end
 end
