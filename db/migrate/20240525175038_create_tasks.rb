b875d7251bb9db49ee083c88221ac42f221d7cc7
jamerson <jcventurosa@gmail.com>
2024-12-31 01:57:50 -0300

    editing migrations due to changes in tasks

--- db/migrate/20240525175038_create_tasks.rb
+++ db/migrate/20240525175038_create_tasks.rb
@@ -2,8 +2,9 @@ class CreateTasks < ActiveRecord::Migration[7.1]
   def change
     create_table :tasks do |t|
       t.string :title
+      t.datetime :term 
+      t.string :member, default: "Jamerson Carlos"
       t.boolean :completed, default: false
-
       t.timestamps
     end
   end
