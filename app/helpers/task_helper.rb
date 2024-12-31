c99692a00cbeced845b99a2055551f05dc2f12e2
jamerson <jcventurosa@gmail.com>
2024-12-31 01:59:07 -0300

    filter helpers

--- app/helpers/task_helper.rb
+++ app/helpers/task_helper.rb
@@ -1,3 +1,4 @@
+require 'date'
 module TaskHelper
     def filter_completed(tasks)
         tasks.filter { |task| task.completed }
@@ -7,8 +8,29 @@ module TaskHelper
         tasks.filter { |task| task.completed == false}
     end
 
+    def filter_tasks_today(tasks) 
+        tasks.filter{|task| verify_status_term(task.term) == 2}
+    end
+
+    def filter_tasks_overdue(tasks) 
+        tasks.filter{|task| verify_status_term(task.term) == 1}
+    end
+  
+
     def transform_format_date(data)
         datetime = DateTime.parse(data.to_s) 
         datetime.strftime('%d/%m/%Y - %H:%M')
     end
+    
+    def verify_status_term(data) 
+        return 0 if data.nil? # Retorna falso se data for nil
+        
+        if data.to_date < Date.today
+            1            
+        elsif data.to_date == Date.today     
+            2
+        end
+        
+    end
+    
 end
