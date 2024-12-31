b3d1aa30040af3fe8b4230cfbcf2cf1ba43013f5
jamerson <jcventurosa@gmail.com>
2024-12-31 01:56:16 -0300

    including dynamic routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,22 +5,16 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   # get "up" => "rails/health#show", as: :rails_health_check
 
-  # Defines the root path route ("/")
-  # root "posts#index"
-  get '/lists',to: 'lists#index'
-  post '/lists', to: 'lists#create'
-  get '/lists/new', to: 'lists#new', as: 'new_list'
-  get '/lists/:id/edit', to: 'lists#edit', as: 'edit_list'
-  get '/lists/:id', to: 'lists#show', as: 'list'
-  patch '/lists/:id', to: 'lists#update'
-  put '/lists/:id', to: 'lists#update'
-  delete '/lists/:id', to: 'lists#destroy'
+  
 
-  get '/show_tasks/:id', to: 'task#show_tasks', as: 'show_tasks'
-  post '/show_tasks/:id', to: 'task#create'
-  delete '/tasks/:id', to: 'task#destroy', as: 'delete_task'
-  put '/tasks/:id', to: 'task#update', as: 'update_task'
-  patch '/tasks/:id', to: 'task#update'
+  
 
   root 'lists#index'
-end
+
+  resources :tasks
+  resources :lists
+
+  put '/tasks/:id', to: 'tasks#update', as: 'update_task'
+  delete '/tasks/:id', to: 'tasks#destroy', as: 'delete_task'
+  delete '/lists/:id', to: 'lists#destroy', as: 'delete_list'
+end 
